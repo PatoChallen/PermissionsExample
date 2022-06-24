4d52fb79d0fe8c330d73f43932a4e4d9124ac176
patochallen <patofch@gmail.com>
2022-06-24 15:44:23 -0300

    Add camera example #5 Accompanist System UI Controller implemented RequestCameraPermissions.kt added RequestPermissions.kt cleaned Material DS implemented

--- app/src/main/java/com/patochallen/permissionsexample/permissions/RequestPermissions.kt
+++ app/src/main/java/com/patochallen/permissionsexample/permissions/RequestPermissions.kt
@@ -1,30 +1,101 @@
+@file:OptIn(ExperimentalPermissionsApi::class)
+
 package com.patochallen.permissionsexample.permissions
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionState
+import com.google.accompanist.permissions.rememberPermissionState
 
-@ExperimentalPermissionsApi
 @Composable
-internal fun RequestPermissions(
-    permissionState: PermissionState,
+fun RequestPermissions(
+    permission: String,
     permissionNotGrantedContent: @Composable (() -> Unit),
     permissionDeniedContent: @Composable (() -> Unit),
     content: @Composable (() -> Unit)
 ) {
+    val context = LocalContext.current
+    val permissionState = rememberPermissionState(permission = permission)
+
     when {
         permissionState.hasPermission -> {
             content()
         }
         permissionState.hasPermissionDenied() -> {
-            permissionDeniedContent()
+            PermissionContentWrapper(
+                buttonText = "Open settings",
+                content = permissionDeniedContent
+            ) {
+                context.goToSettings()
+            }
         }
         else -> {
-            permissionNotGrantedContent()
+            PermissionContentWrapper(
+                buttonText = "Request camera permissions",
+                content = permissionNotGrantedContent
+            ) {
+                permissionState.launchPermissionRequest()
+            }
         }
     }
 }
 
-@ExperimentalPermissionsApi
 private fun PermissionState.hasPermissionDenied() =
     permissionRequested && shouldShowRationale.not()
+
+private fun Context.goToSettings() {
+    startActivity(
+        Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.fromParts("package", packageName, null)
+        )
+    )
+}
+
+@Composable
+private fun PermissionContentWrapper(
+    buttonText: String,
+    content: @Composable (() -> Unit),
+    onClick: () -> Unit
+) {
+    Column(modifier = Modifier.padding(32.dp)) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            content()
+        }
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth(),
+            shape = CircleShape,
+            contentPadding = PaddingValues(15.dp)
+        ) {
+            Text(
+                text = buttonText.uppercase(),
+                style = MaterialTheme.typography.button,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
+    }
+}
--- app/src/main/java/com/patochallen/permissionsexample/ui/theme/Color.kt
+++ app/src/main/java/com/patochallen/permissionsexample/ui/theme/Color.kt
@@ -4,7 +4,7 @@ package com.patochallen.permissionsexample.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Yellow500 = Color(0xFFFFEB3B)
+val Yellow800 = Color(0xFFFFC107)
+
+val Gray800 = Color(0xFF141414)
