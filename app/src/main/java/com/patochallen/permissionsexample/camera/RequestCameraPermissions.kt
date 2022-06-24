5b88f8f5bb3f0c926b217fa3ba03b1b6de3581aa
Patricio Challen <37918735+PatoChallen@users.noreply.github.com>
2022-06-24 15:49:05 -0300

    Add camera example #5 (#6)
    Accompanist System UI Controller implemented
    RequestCameraPermissions.kt added
    RequestPermissions.kt cleaned
    Material DS implemented

---
+++ app/src/main/java/com/patochallen/permissionsexample/camera/RequestCameraPermissions.kt
@@ -0,0 +1,166 @@
+package com.patochallen.permissionsexample.camera
+
+import android.Manifest.permission
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.AccountCircle
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.patochallen.permissionsexample.R.drawable
+import com.patochallen.permissionsexample.permissions.RequestPermissions
+import com.patochallen.permissionsexample.ui.theme.onWarning
+import com.patochallen.permissionsexample.ui.theme.warning
+
+@Composable
+fun RequestCameraPermissions() {
+    RequestPermissions(
+        permission = permission.CAMERA,
+        permissionNotGrantedContent = { PermissionNotGrantedContent() },
+        permissionDeniedContent = { PermissionDeniedContent() }
+    ) {
+        PermissionGrantedContent()
+    }
+}
+
+@Composable
+fun PermissionNotGrantedContent() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Box(
+            modifier = Modifier
+                .size(250.dp)
+                .shadow(15.dp, CircleShape, false)
+                .background(MaterialTheme.colors.primary, CircleShape)
+                .padding(10.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = drawable.camera),
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .offset(x = 30.dp, y = 20.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(70.dp))
+        Text(
+            text = "Enable Camera",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        Text(
+            text = "Please provide access to\nyour camera, which is required\nfor use the App",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            style = MaterialTheme.typography.subtitle1,
+            textAlign = TextAlign.Center,
+            lineHeight = 25.sp
+        )
+    }
+}
+
+@Composable
+fun PermissionDeniedContent() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Box(
+            modifier = Modifier
+                .size(250.dp)
+                .shadow(15.dp, CircleShape, false)
+                .background(MaterialTheme.colors.primary, CircleShape)
+                .padding(10.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = drawable.camera),
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .offset(x = 30.dp, y = 20.dp)
+            )
+            Image(
+                painter = painterResource(id = drawable.ic_warning),
+                contentDescription = "",
+                modifier = Modifier
+                    .size(100.dp)
+                    .align(Alignment.TopStart)
+                    .offset(x = 25.unaryMinus().dp, y = 25.unaryMinus().dp)
+                    .shadow(5.dp, CircleShape, false)
+                    .background(MaterialTheme.colors.warning, CircleShape)
+                    .padding(15.dp)
+                    .padding(bottom = 7.dp),
+                colorFilter = ColorFilter.tint(color = MaterialTheme.colors.onWarning)
+            )
+        }
+        Spacer(modifier = Modifier.height(70.dp))
+        Text(
+            text = "Enable Camera",
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        Text(
+            text = "The permission has been denied\ntwo times, please open settings\nand grant permissions manually",
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(2.dp, MaterialTheme.colors.warning, RoundedCornerShape(10.dp))
+                .padding(20.dp),
+            style = MaterialTheme.typography.subtitle1,
+            textAlign = TextAlign.Center,
+            lineHeight = 25.sp
+        )
+    }
+}
+
+@Composable
+private fun PermissionGrantedContent() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        IconButton(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 50.dp),
+            onClick = {},
+            content = {
+                Icon(
+                    imageVector = Icons.Sharp.AccountCircle,
+                    contentDescription = "Take picture",
+                    tint = Color.White,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(1.dp)
+                        .border(1.dp, Color.White, CircleShape)
+                )
+            }
+        )
+    }
+}
--- app/src/main/java/com/patochallen/permissionsexample/ui/theme/Theme.kt
+++ app/src/main/java/com/patochallen/permissionsexample/ui/theme/Theme.kt
@@ -1,31 +1,22 @@
 package com.patochallen.permissionsexample.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+private val DarkColorPalette = darkColors()
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val LightColorPalette = lightColors()
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
+val Colors.warning: Color
+    get() = if (!isLight) Yellow500 else Yellow800
+
+val Colors.onWarning: Color get() = Gray800
 
 @Composable
 fun PermissionsExampleTheme(
@@ -37,6 +28,10 @@ fun PermissionsExampleTheme(
     } else {
         LightColorPalette
     }
+
+    rememberSystemUiController().setSystemBarsColor(
+        color = colors.surface
+    )
 
     MaterialTheme(
         colors = colors,
