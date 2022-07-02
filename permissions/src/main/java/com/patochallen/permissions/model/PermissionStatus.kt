8351366c2a78e41bb128c3b5572d1f74f445756b
patochallen <patofch@gmail.com>
2022-07-02 20:21:17 -0300

    Remove Unchecked from PermissionStatus #16 Unchecked removed from PermissionStatus

--- permissions/src/main/java/com/patochallen/permissions/model/PermissionStatus.kt
+++ permissions/src/main/java/com/patochallen/permissions/model/PermissionStatus.kt
@@ -1,17 +1,19 @@
 package com.patochallen.permissions.model
 
 import androidx.compose.runtime.Stable
+import com.patochallen.permissions.model.PermissionStatus.Denied
+import com.patochallen.permissions.model.PermissionStatus.Granted
+import com.patochallen.permissions.model.PermissionStatus.ShowRational
 
 /**
  * Model of the status of a permission.
- * It can be [Granted], [Denied], [ShowRational] or [Unrequested].
+ * It can be [Granted], [ShowRational] or [Denied].
  * If denied, it should show a rationale to the user.
  */
 @ExperimentalApi
 @Stable
 sealed interface PermissionStatus {
     object Granted : PermissionStatus
-    object Denied : PermissionStatus
     object ShowRational : PermissionStatus
-    object Unrequested : PermissionStatus
+    object Denied : PermissionStatus
 }
