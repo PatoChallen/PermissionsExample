1a399ce775b9ebb0d3fa0b9439da370d1ec9fc11
patochallen <patofch@gmail.com>
2022-07-02 23:40:27 -0300

    Rename app module to sample #22 app module renamed to sample android.disableAutomaticComponentCreation=true added to gradle.properties accompanist systemuicontroller removed Theme simplified, Shape, Color and Type removed README updated

--- sample/src/main/java/com/patochallen/permissionsexample/ui/theme/Theme.kt
+++ sample/src/main/java/com/patochallen/permissionsexample/ui/theme/Theme.kt
@@ -2,14 +2,11 @@ package com.patochallen.permissionsexample.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
+import androidx.compose.material.Typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
-
-private val DarkColorPalette = darkColors()
-
-private val LightColorPalette = lightColors()
 
 @Composable
 fun PermissionsExampleTheme(
@@ -17,19 +14,15 @@ fun PermissionsExampleTheme(
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        darkColors()
     } else {
-        LightColorPalette
+        lightColors()
     }
-
-    rememberSystemUiController().setSystemBarsColor(
-        color = colors.surface
-    )
 
     MaterialTheme(
         colors = colors,
-        typography = Typography,
-        shapes = Shapes,
+        typography = Typography(),
+        shapes = Shapes(),
         content = content
     )
 }
