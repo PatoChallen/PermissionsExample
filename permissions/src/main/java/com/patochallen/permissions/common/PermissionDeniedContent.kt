9bbfcfe5ac0f06f9e9021547a650f732fe34913b
patochallen <patofch@gmail.com>
2022-07-06 21:51:13 -0300

    Remove warning #34 warning icon removed

--- permissions/src/main/java/com/patochallen/permissions/common/PermissionDeniedContent.kt
+++ permissions/src/main/java/com/patochallen/permissions/common/PermissionDeniedContent.kt
@@ -4,13 +4,10 @@ import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -21,13 +18,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.patochallen.permissions.R.drawable
 
 @Composable
 internal fun PermissionDeniedContent(
@@ -42,32 +37,13 @@ internal fun PermissionDeniedContent(
         onClick = onClick
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Box(
+            Image(
+                painter = painterResource(id = iconId),
+                contentDescription = "Permission Icon",
                 modifier = Modifier
                     .size(250.dp)
-                    .shadow(15.dp, CircleShape, false),
-                contentAlignment = Alignment.Center
-            ) {
-                Image(
-                    painter = painterResource(id = iconId),
-                    contentDescription = "Permission Icon",
-                    modifier = Modifier
-                        .fillMaxSize()
-                )
-                Image(
-                    painter = painterResource(id = drawable.ic_warning),
-                    contentDescription = "Warning Icon",
-                    modifier = Modifier
-                        .size(100.dp)
-                        .align(Alignment.TopStart)
-                        .offset(x = 25.unaryMinus().dp, y = 25.unaryMinus().dp)
-                        .shadow(5.dp, CircleShape, false)
-                        .background(MaterialTheme.colors.error, CircleShape)
-                        .padding(15.dp)
-                        .padding(bottom = 5.dp),
-                    colorFilter = ColorFilter.tint(color = MaterialTheme.colors.onError)
-                )
-            }
+                    .shadow(15.dp, CircleShape)
+            )
             Spacer(modifier = Modifier.height(70.dp))
             Text(
                 text = permissionTitle,
