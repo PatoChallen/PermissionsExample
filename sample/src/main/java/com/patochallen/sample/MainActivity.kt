61959fd27b28afb38f15bdeaf7326fd45f2db2a7
Patricio Challen <37918735+patochallen@users.noreply.github.com>
2022-07-10 03:07:43 -0300

    Create contacts permission sample #57 (#58)
    RequestContactsPermission created
    strings and icon added

--- sample/src/main/java/com/patochallen/sample/MainActivity.kt
+++ sample/src/main/java/com/patochallen/sample/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.patochallen.permissions.camera.RequestCameraPermission
+import com.patochallen.permissions.contacts.RequestContactsPermission
 import com.patochallen.permissions.location.RequestLocationPermission
 import com.patochallen.permissions.microphone.RequestMicrophonePermission
 import com.patochallen.permissions.model.ExperimentalPermissionApi
@@ -22,14 +25,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             MaterialTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(15.dp),
                     color = MaterialTheme.colors.background
                 ) {
-                    RequestLocationPermission {
-                        RequestMicrophonePermission {
-                            RequestCameraPermission {
-                                Box(contentAlignment = Alignment.Center) {
-                                    Text(text = "PERMISSIONS GRANTED")
+                    RequestContactsPermission {
+                        RequestLocationPermission {
+                            RequestMicrophonePermission {
+                                RequestCameraPermission {
+                                    Box(contentAlignment = Alignment.Center) {
+                                        Text(text = "PERMISSIONS GRANTED")
+                                    }
                                 }
                             }
                         }
